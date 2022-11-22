8907c9793acb46cba1e7e784a48f7197bcb9db46
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2022-11-22 12:33:18 +0100

    add server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import path from 'path';
 import jsonServer from 'json-server';
 
@@ -7,6 +8,7 @@ const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
 });
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
